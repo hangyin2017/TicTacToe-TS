d5e29e53e6ea736d2ae8ad6c07b175f109707bea
Hang <hang.yin2017@outlook.com>
2021-01-25 21:07:22 +1100

    modify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 module.exports = {
   entry: './src/app.jsx',
   output: {
-    filename: './bundle.js'
+    filename: './dist/bundle.js'
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
     rules: [
-      { test: /\.jsx?$/, exclude: /node_modules/, use: { loader: 'babel-loader' } }
+      { test: /\.(t|j)sx?$/, exclude: /node_modules/, use: { loader: 'ts-loader' } },
+      { enforce: "pre", test: /\.js$/, exclude: /node_modules/, loader: "source-map-loader" },
     ]
   },
   externals: {
     "react": "React",
     "react-dom": "ReactDOM",
   },
+  devtool: "source-map",
 }
